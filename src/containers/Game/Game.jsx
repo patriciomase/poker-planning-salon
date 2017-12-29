fe1c146b1bf82b56827d7dfeb220bc152ad76e16
Mauro Gaston Laterra <mlaterra@sparkdigital.com>
2017-12-28 22:41:10 -0300

    removed header and fixed navbar styles

--- src/containers/Game/Game.jsx
+++ src/containers/Game/Game.jsx
@@ -24,19 +24,10 @@ class Game extends Component {
   
   render() {
     const { users } = this.props;
-    const userList = users.map(user => (
-      <div className="game-user" key={user.id}>
-        <img src="https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/18057645_10211905023837405_2513766658459686163_n.jpg?oh=4a2f6aca2b59d76fe7b4732e08cea6b5&oe=59DEB9B5" />
-        <span className="game-user-name">
-          {user.name}
-        </span>
-      </div>
-    ));
 
     return (
       <div className="game">
         <TicketName {...this.props.currentTicket} />
-        {userList}
           <Card value={1} clickAction={() => {}} />
           <Card value={3} clickAction={() => {}} />
           <Card value={5} clickAction={() => {}} />
