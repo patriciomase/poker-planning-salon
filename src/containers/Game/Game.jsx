89ad56cceba43c8308e8fd37c6398218c7635e89
Mauro Gaston Laterra <mlaterra@sparkdigital.com>
2018-01-08 09:06:10 -0300

    added hamburger menu in the header navbar

--- src/containers/Game/Game.jsx
+++ src/containers/Game/Game.jsx
@@ -22,36 +22,9 @@ class Game extends Component {
   }
 
   render() {
-    // const { users } = this.props;
-
-    const sidebarOpen = () => {
-      document.getElementById('main').style.marginLeft = '200px';
-      document.getElementById('mySidebar').style.width = '200px';
-      document.getElementById('mySidebar').style.display = 'block';
-    };
-
-    const sidebarClose = () => {
-      document.getElementById('mySidebar').style.display = 'none';
-    };
 
     return (
       <div>
-        <div className="w3-sidebar w3-bar-block w3-collapse w3-card sidebar" id="mySidebar">
-          <button className="w3-bar-item w3-button w3-hide-large" onClick={sidebarClose}>
-            Close &times;
-          </button>
-          <a href="#" className="w3-bar-item w3-button">Link 1</a>
-          <a href="#" className="w3-bar-item w3-button">Link 2</a>
-          <a href="#" className="w3-bar-item w3-button">Link 3</a>
-        </div>
-
-        <div className="w3-main content" id="main">
-
-          <div className="w3-teal">
-            <button className="w3-button w3-teal w3-xlarge w3-right" onClick={sidebarOpen}>&#9776;</button>
-            <h1>My Page</h1>
-          </div>
-        </div>
       </div>
     );
   }
